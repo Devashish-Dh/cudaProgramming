2ee8c87224b72a9f321c22ad294f935aa79f8204
= <=>
2024-10-05 17:56:51 +0530

    got the headers working with cuda and cmake, nice!

---
+++ Cmake_template_for_CUDA/src/h1.cu
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "h1.cuh"
+
+__global__ 
+void myHelloKernel(){
+
+}
+
--- Cmake_template_for_CUDA/src/hello.cu
+++ Cmake_template_for_CUDA/src/hello.cu
@@ -1,16 +1,17 @@
 #include <iostream>
+#include "h1.cuh"
 
-__global__ void myKernel(void) {}
+//__global__ void myKernel(void) {}
 
 int main()
 {
+
 std::cout << "hello cuda and C / C++ together ! \n";
 
+myHelloKernel <<< 1, 1 >>>();
 
-myKernel <<< 1, 1 >>>();
 std::cout << "Hello CUDA!\n";
 
 return 0;
-
 
 }
